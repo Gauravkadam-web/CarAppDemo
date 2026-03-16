9fa0e35a6a836eb576164af54838fdec43445bf5
GAURAVKADAM007 <gkadam3847@gmail.com>
2026-03-16 13:08:56 +0530

    Initial CarApp V1

---
+++ src/main/java/com/stackninjas/Battery.java
@@ -0,0 +1,7 @@
+package com.stackninjas;
+
+public class Battery {
+    public void supply(){
+        System.out.println("Power is Supplied...🔋🔋");
+    }
+}
---
+++ src/main/java/com/stackninjas/Engine.java
@@ -0,0 +1,8 @@
+package com.stackninjas;
+
+public class Engine {
+    public void start(){
+        System.out.println("Engine is Started....🚂🚂");
+    }
+
+}
